d83f41177bfcb815343f07b93382d32116d6ee1b
Maneeshraja19 <maneeshraja47@gmail.com>
2026-08-01 23:27:09 -0400

    Add reusable skills list module

---
+++ src/skills_list.py
@@ -0,0 +1,20 @@
+# Curated list of common data analyst/scientist skills and tools
+SKILLS_LIST = [
+    # Programming languages
+    'python', 'sql', 'r', 'java', 'scala', 'c++',
+    # Data tools
+    'excel', 'tableau', 'power bi', 'looker', 'qlik',
+    # Databases
+    'mysql', 'postgresql', 'mongodb', 'snowflake', 'redshift', 'bigquery',
+    # ML/Stats libraries
+    'pandas', 'numpy', 'scikit-learn', 'tensorflow', 'pytorch', 'keras',
+    # Big data
+    'spark', 'hadoop', 'kafka', 'airflow',
+    # Cloud
+    'aws', 'azure', 'gcp', 'google cloud',
+    # Concepts
+    'machine learning', 'deep learning', 'statistics', 'a/b testing',
+    'data visualization', 'etl', 'data warehousing', 'nlp',
+    # Soft/business skills often listed
+    'stakeholder management', 'communication', 'agile', 'scrum'
+]
